a2cd08a2950dbef0a9e119fb6a1182c0431b26f4
Gabriel Quadra <gabriel.quadra@edu.es.senac.br>
2026-09-16 16:55:51 -0300

    cria variavel para usar depois

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// Este programa deverá salvar o nome do usuário dentro de uma variavel
+// Este programa deverá exibir uma mensagem de saudação mostrando o nome do usuário
+var nomeUsuario = "Quadra";
+Console.Write("Boas vindas, ");
+Console.WriteLine(nomeUsuario);
